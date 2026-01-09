2b7892b2a78a15da5f9095dde441a50419be056d
bonnatsu <mgwt0mgj@icloud.com>
2026-01-10 00:33:02 +0900

    20260109 add

--- script/users_sample.py
+++ script/users_sample.py
@@ -1,8 +1,10 @@
 import csv
 import uuid
+from pathlib import Path
+BASE_DIR = Path(__file__).resolve().parent
 
+output_file = BASE_DIR / "users_sample.csv"
 row_count = 10000
-output_file = "users_sample.csv"
 
 with open(output_file,"w",newline="",encoding="utf-8") as f:
     writer = csv.writer(f)
