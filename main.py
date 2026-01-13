841424aa670f99b362888a643f447df7ab4de194
谷口光太郎 <taniguchikoutarou@taniguchikoutarounoMacBook-Air.local>
2026-01-14 00:13:25 +0900

    main.py add

---
+++ main.py
@@ -0,0 +1,63 @@
+from fastapi import FastAPI, HTTPException
+from pydantic import BaseModel
+from db import get_conn
+
+app = FastAPI()
+class StockInRequest(BaseModel):
+    id: int
+    quantity: int
+
+@app.post("/stock/in")
+def stock_in(req: StockInRequest):
+
+    if req.quantity <= 0:
+        raise HTTPException(status_code=400,detail="quantity must be > 0")
+    
+    conn = get_conn()
+    cur = conn.cursor()
+
+
+    try:
+        cur.execute(
+            "SELECT 1 FROM products WHERE id = %s",
+            (req.id,)
+        )
+        if cur.fetchone() is none:
+            raise HTTPException(status_code=404, detail="producta not found")
+        
+        cur.execute(
+            "SELECT quantity FROM stocks WHERE id = %s FOR UPDATE",
+            (req.id,)
+        )
+        row = cur.fetchone()
+
+        if row is None:
+            #在庫なければ作る
+            cur.execute(
+                "INSERT INTO stocks (id,quantity) VALUES (%s,%s)",
+                (req.id,req.quantity)
+            )
+            new_qty = req.quantity
+        else:
+            cur.execute(
+                "UPDATE stocks SET quantity = quantity + %s WHERE id = %s",
+                (req.quantity,req.id)
+            )
+            new_qty = row[0] + req + req.quantity
+        
+        conn.commit()
+
+        return {
+            "id": req.id,
+            "quantity": req.quantity
+        }
+    
+    except HTTPException:
+        conn.rollback()
+        raise
+    except Exception as e:
+        conn.rollback()
+        raise HTTPException(status_code=500,detail=str(e))
+    finally:
+        cur.close()
+        conn.close()
