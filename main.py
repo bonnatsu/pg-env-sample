c50755ca17582ad44b038e330355cc01653cca67
谷口光太郎 <taniguchikoutarou@taniguchikoutarounoMacBook-Air.local>
2026-01-14 23:56:28 +0900

    richmodify

--- main.py
+++ main.py
@@ -79,3 +79,73 @@ def stock_in_page(request: Request):
         "stock_in.html",
         {"request": request}
     )
+
+
+
+@app.post("/stock/out")
+def stock_out(req: StockInRequest):
+
+    if req.quantity <= 0:
+        raise HTTPException(status_code=400,detail="quantity must be > 0")
+    
+    conn = get_conn()
+    cur = conn.cursor()
+
+
+    try:
+        cur.execute(
+            "SELECT product_name FROM products WHERE id = %s",
+            (req.id,)
+        )
+        row = cur.fetchone()
+        if row is None:
+            raise HTTPException(status_code=404, detail="product not found")
+        
+        product_name = row[0]
+        
+        cur.execute(
+            "SELECT quantity FROM stocks WHERE product_id = %s FOR UPDATE",
+            (req.id,)
+        )
+        row = cur.fetchone()
+
+        if row is None:
+            #在庫なければ作る
+            cur.execute(
+                "INSERT INTO stocks (product_id,quantity) VALUES (%s,%s)",
+                (req.id,req.quantity)
+            )
+            new_qty = req.quantity
+        else:
+            cur.execute(
+                "UPDATE stocks SET quantity = quantity - %s WHERE product_id = %s",
+                (req.quantity,req.id)
+            )
+            new_qty = row[0] + req.quantity
+        
+        conn.commit()
+
+        return {
+            "id": req.id,
+            "product_name":product_name,
+            "quantity": req.quantity,
+            "current_quantity": new_qty,
+            "message": f"商品名:{product_name} 出庫完了"
+        }
+    
+    except HTTPException:
+        conn.rollback()
+        raise
+    except Exception as e:
+        conn.rollback()
+        raise HTTPException(status_code=500,detail=str(e))
+    finally:
+        cur.close()
+        conn.close()
+
+@app.get("/stock/out-page", response_class=HTMLResponse)
+def stock_out_page(request: Request):
+    return templates.TemplateResponse(
+        "stock_out.html",
+        {"request": request}
+    )
